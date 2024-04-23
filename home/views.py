e78f0a382e66575a30bdd8fc0250c25de0100a8d
Al2661997 <167825084+Al2661997@users.noreply.github.com>
2024-04-23 03:21:09 +0200

    Add files via upload

---
+++ home/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+# Slack cloning
+def home(request):
+    return render(request, 'home.html')
+
+
+
+def Apple(request):
+    return render(request, 'apple.html')
+
+
+
+
+
+
